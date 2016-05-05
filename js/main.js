0f6b2d003dba0cd697f5f4344020e34b43932bff
Nicas Lee <nicas1116@gmail.com>
2016-05-05 15:53:07 +0800

    dsa
    das

--- js/main.js
+++ js/main.js
@@ -11,13 +11,15 @@ var recording=false
 
   function recordSound() {
 	  if(recording){
-		recorder && recorder.stop();
+		//recorder && recorder.stop();
+		myMedia.startRecord();
 		$(".btn-record").html("START RECORD")
 		createDownloadLink();
 		recorder.clear();
 		recording=false
 	  }else{
-		recorder && recorder.record();
+		//recorder && recorder.record();
+		myMedia.startRecord();
 	    $(".btn-record").html("STOP RECORD")
 		recording=true;
 	  }
@@ -73,3 +75,19 @@ var recording=false
    
     });
   };
+
+  var myMedia;
+document.addEventListener("deviceready", onDeviceReady, false);
+function onDeviceReady() {
+    alert(Media);
+	myMedia = new Media("documents://beer.mp3",function() {
+		alert("recordAudio():Audio Success");
+		myMedia.play();
+	},
+
+	// error callback
+	function(err) {
+		alert("recordAudio():Audio Error: "+ err.code);
+	});
+
+}
