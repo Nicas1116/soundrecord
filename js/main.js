22702b86d9ade50350cb7e7b4dd43038407f3c94
Nicas Lee <nicas1116@gmail.com>
2016-05-05 15:58:38 +0800

    das
    dsa

--- js/main.js
+++ js/main.js
@@ -1,15 +1,4 @@
-
-  var audio_context;
-  var recorder;
-var recording=false
-  function startUserMedia(stream) {
-    var input = audio_context.createMediaStreamSource(stream);
-    recorder = new Recorder(input, {
-                  numChannels: 1
-                });
-  }
-
-  function recordSound() {
+function recordSound() {
 	  if(recording){
 		//recorder && recorder.stop();
 		myMedia.startRecord();
@@ -26,55 +15,6 @@ var recording=false
 	 
 	  
   }
-
-  function createDownloadLink() {
-    recorder && recorder.exportWAV(function(blob) {
-		console.log(blob)
-      /*var url = URL.createObjectURL(blob);
-      var li = document.createElement('li');
-      var au = document.createElement('audio');
-      var hf = document.createElement('a');
-
-      au.controls = true;
-      au.src = url;
-      hf.href = url;
-      hf.download = new Date().toISOString() + '.wav';
-      hf.innerHTML = hf.download;
-      li.appendChild(au);
-      li.appendChild(hf);
-      recordingslist.appendChild(li);*/
-    });
-  }
-
-  window.onload = function init() {
-    try {
-      // webkit shim
-      window.AudioContext = window.AudioContext || window.webkitAudioContext;
-      navigator.getUserMedia = ( navigator.getUserMedia ||
-                       navigator.webkitGetUserMedia ||
-                       navigator.mozGetUserMedia ||
-                       navigator.msGetUserMedia);
-      window.URL = window.URL || window.webkitURL;
-
-      audio_context = new AudioContext;
-      
-    } catch (e) {
-      alert('No web audio support in this browser!');
-    }
-	var capture = navigator.device.capture;
-	navigator.microphone(function (on) {
-	  if (on) {
-		// now you can use the Cordova media plugin to record
-		startUserMedia()
-	  }
-	  else {
-		// instruct how to enable your app's access to the microphone
-	  }
-	});
-    navigator.getUserMedia({audio: true}, startUserMedia, function(e) {
-   
-    });
-  };
 
   var myMedia;
 document.addEventListener("deviceready", onDeviceReady, false);
